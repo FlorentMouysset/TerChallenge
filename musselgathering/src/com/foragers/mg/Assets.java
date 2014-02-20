eaa2b3c06f8030890017e7e4619e58def702db48
dam4256 <arondeldamien@orange.fr>
2014-02-20 17:13:34 +0100

    #4 Nage fonctionnelle

--- musselgathering/src/com/foragers/mg/Assets.java
+++ musselgathering/src/com/foragers/mg/Assets.java
@@ -2,10 +2,12 @@ package com.foragers.mg;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class Assets {
 	
 	public static Texture background;
+	public static TextureRegion backgroundRegion;
 
 	public static Texture diverUp;
 	public static Texture diverDown;
@@ -17,6 +19,7 @@ public class Assets {
 	
 	public static void load() {
 		background = loadTexture("data/background.png");
+		backgroundRegion = new TextureRegion(background, 0, 0, 320, 480);
 		
 		diverUp = loadTexture("data/diverUp.png");
 		diverDown = loadTexture("data/diverDown.png");
