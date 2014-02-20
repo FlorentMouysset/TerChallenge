4ef00131924e57f97faa36c7958e38c94e50b818
Florent Mouysset <mouysset.florent@gmail.com>
2014-02-20 09:27:41 +0100

    #7 #8 #9 add methods signatures

---
+++ src/com/foragers/terchallange/score/ScoreManagerImpl.java
@@ -0,0 +1,31 @@
+package com.foragers.terchallange.score;
+
+public class ScoreManagerImpl implements IScoreManager{
+
+	@Override
+	public void AddShellToScore() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void diveUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getScore() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getDioxygenDelayRest(int deltaTime) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+}
