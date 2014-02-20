e8b88db47f0630dc62e3f6022de8a5ae2b27b9b3
Florent Mouysset <mouysset.florent@gmail.com>
2014-02-20 09:57:08 +0100

    #6 #7 correctif

--- src/com/foragers/terchallange/logic/Breath.java
+++ src/com/foragers/terchallange/logic/Breath.java
@@ -3,13 +3,21 @@ package com.foragers.terchallange.logic;
 public class Breath {
 
 	private Oxygen oxygen;
+	private float breathLevel;
 	
 	public Breath(Oxygen oxygen){
 		this.oxygen = oxygen;
 	}
 	
-	public void diveUp() {
-		oxygen.resetOxygenLevel();
+	public float getBreathLevel(float deltaTime){
+		breathLevel = breathLevel - deltaTime*0.1f;
+		return breathLevel;
+	}
+	
+	
+	public void takeABreath(){
+		this.oxygen.takeABreath();
+		breathLevel = 10;
 	}
 	
 }
--- src/com/foragers/terchallange/logic/Oxygen.java
+++ src/com/foragers/terchallange/logic/Oxygen.java
@@ -4,13 +4,16 @@ public class Oxygen {
 
 	private float oxygenLevel;
 	
-	public float getDioxygenDelayRest(float deltaTime) {
-		oxygenLevel = oxygenLevel - deltaTime*0.1f;
+	public float getDioxygenLevel() {
 		return oxygenLevel;
 	}
 	
 	public void resetOxygenLevel(){
 		oxygenLevel = 100;
+	}
+
+	public void takeABreath() {
+		oxygenLevel -= 10;
 	}
 	
 }
