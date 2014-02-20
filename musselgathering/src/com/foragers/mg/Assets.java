39ffdac135689a7c1424d9d062addad755dce3c6
dam4256 <arondeldamien@orange.fr>
2014-02-20 14:39:51 +0100

    Suppression des ressources non fonctionnelles

--- musselgathering/src/com/foragers/mg/Assets.java
+++ musselgathering/src/com/foragers/mg/Assets.java
@@ -8,11 +8,7 @@ public class Assets {
 	
 	public static Texture background;
 	public static TextureRegion backgroundRegion;
-	
-	public static Texture title;
-	
-	public static Texture items;
-	public static Animation bubbles;
+
 	public static Texture mussel;
 	
 	public static Texture loadTexture(String file) {
@@ -23,10 +19,6 @@ public class Assets {
 		background = loadTexture("data/background.png");
 		backgroundRegion = new TextureRegion(background, 0, 0, 320, 480);
 		
-		title = loadTexture("data/title.png");
-		
-		items = loadTexture("data/bubbles.png");
-		bubbles = new Animation(0.2f, new TextureRegion(items, 0, 0, 8, 8), new TextureRegion(items, 8, 0, 8, 8), new TextureRegion(items, 24, 0, 8, 8), new TextureRegion(items, 8, 0, 8, 8));
 		mussel = loadTexture("data/mussel.png");
 	}
 
