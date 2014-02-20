dd84a5787e4f3cc1cbe7a8574e45442c7274f79f
dam4256 <arondeldamien@orange.fr>
2014-02-20 15:18:50 +0100

    #1 Génération des moules OK

--- musselgathering/src/com/foragers/mg/entities/Mussel.java
+++ musselgathering/src/com/foragers/mg/entities/Mussel.java
@@ -1,12 +1,9 @@
 package com.foragers.mg.entities;
 
 public class Mussel extends Entity {
-
-	public static final float MUSSEL_WIDTH = 0.3f;
-	public static final float MUSSEL_HEIGHT = 0.3f;
 	
 	public Mussel(float x, float y) {
-		super(x, y, MUSSEL_WIDTH, MUSSEL_HEIGHT);
+		super(x, y);
 	}
 
 }
--- musselgathering/src/com/foragers/mg/entities/World.java
+++ musselgathering/src/com/foragers/mg/entities/World.java
@@ -6,8 +6,8 @@ import java.util.Random;
 
 public class World {
 
-	public static final float WORLD_WIDTH = 10;
-	public static final float WORLD_HEIGHT = 15 * 20;
+	public static final float WORLD_WIDTH = 9.5f;
+	public static final float WORLD_HEIGHT = 14;
 	public static final int WORLD_STATE_RUNNING = 0;
 	public static final int WORLD_STATE_GAME_OVER = 1;
 	
@@ -26,8 +26,8 @@ public class World {
 
 	private void generateLevel() {
 		for (int i = 0; i < 10; i++) {
-			float x = rand.nextFloat() * (WORLD_WIDTH - Mussel.MUSSEL_WIDTH) + Mussel.MUSSEL_WIDTH / 2;
-			float y = rand.nextFloat() * (WORLD_HEIGHT - Mussel.MUSSEL_HEIGHT) + Mussel.MUSSEL_HEIGHT / 2;
+			float x = rand.nextFloat() * WORLD_WIDTH;
+			float y = rand.nextFloat() * WORLD_HEIGHT;
 			mussels.add(new Mussel(x, y));
 		}
 	}
